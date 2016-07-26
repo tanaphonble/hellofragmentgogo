5b2409ffa20d98b65b0c29c3581c2f42b1f9340c
Tanaphon <tanaphon.a@augmentis.biz>
2016-07-27 00:55:49 +0700

    test fragment transaction

--- app/src/main/java/com/augmentis/ayp/hellofragmentgogo/MainActivity.java
+++ app/src/main/java/com/augmentis/ayp/hellofragmentgogo/MainActivity.java
@@ -1,23 +1,59 @@
 package com.augmentis.ayp.hellofragmentgogo;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyFragment myFragment = new MyFragment();
+        Button buttonOne = (Button) findViewById(R.id.btn_one);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OneFragment oneFragment = new OneFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, oneFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.layout_fragment_container, myFragment);
-        transaction.commit();
+        Button buttonTwo = (Button) findViewById(R.id.btn_two);
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TwoFragment twoFragment = new TwoFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, twoFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        Button buttonThree = (Button) findViewById(R.id.btn_three);
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ThreeFragment threeFragment = new ThreeFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, threeFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/augmentis/ayp/hellofragmentgogo/OneFragment.java
@@ -0,0 +1,28 @@
+package com.augmentis.ayp.hellofragmentgogo;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class OneFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_one_fragment, container, false);
+        Button buttonClose = (Button) rootView.findViewById(R.id.btn_close);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().remove(OneFragment.this).commit();
+            }
+        });
+
+        return rootView;
+    }
+}
